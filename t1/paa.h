6e6ef4ace7f215e7e4498f60f21a95ba596e8de0
Felipe cruz <felipecruz@loogica.net>
2013-09-13 18:46:49 -0300

    Move mergesort to paa.h

--- t1/paa.h
+++ t1/paa.h
@@ -9,3 +9,34 @@ void print_array (int array[], int length) {
 
     printf ("\n");
 }
+
+void _mergesort (int *array, int length) {
+    int copy_array[length];
+    int middle = length / 2;
+    int c, i, j;
+
+    c = i = 0;
+    j = middle;
+
+    if (length == 1) {
+        return;
+    }
+
+    _mergesort (array, middle);
+    _mergesort (&array[middle], length - middle);
+
+    while (c < length) {
+        if (array[i] < array[j] && i < middle) {
+            copy_array[c] = array[i++];
+        } else if (array [i] > array[j] && j < length) {
+            copy_array[c] = array[j++];
+        } else if (i >= middle) {
+            copy_array[c] = array[j++];
+        } else if (j >= length) {
+            copy_array[c] = array[i++];
+        }
+        c++;
+    }
+
+    memcpy (array, copy_array, length * sizeof (int));
+}
