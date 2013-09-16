553b78cd3a933a195b5e4d6b807e6d18a528a747
Felipe cruz <felipecruz@loogica.net>
2013-09-16 16:25:36 -0300

    Improve generator feedback

--- t1/pygen.py
+++ t1/pygen.py
@@ -4,17 +4,20 @@ def gen_list(size):
     f = open('py%d.txt' % size, 'w')
     i = 0
     number_tracker = {}
+
     while i < size:
         number = random.randint(1, size)
         if not number in number_tracker:
             f.write('%d\n' % number)
             i += 1
             number_tracker[number] = True
+        if i % 1000 == 0:
+            print("Missing %d" % (size - i))
 
     f.close()
 
 if __name__ == "__main__":
-    for i in range(10):
+    for i in range(1, 16, 1):
         size = 1000 * (2 ** i)
-        print(size)
+        print("Instance %d size: %d" % (i, size))
         gen_list(size)
