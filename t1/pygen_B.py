afdfed099d51201fb3066c33533d21848dc2e3db
Felipe cruz <felipecruz@loogica.net>
2013-09-24 11:22:38 -0300

    Fix type B generator numbers - from 1 to 2 ** 25

--- t1/pygen_B.py
+++ t1/pygen_B.py
@@ -5,20 +5,20 @@ def gen_list(size):
     size = 2 ** 25
     numbers = []
     for i in xrange(size):
-        numbers.append(i)
+        numbers.append(i+1)
     for i in xrange(swaps):
-        rand_index_1 = random.randint(1, size - 1)
-        rand_index_2 = random.randint(1, size - 1)
+        rand_index_1 = random.randint(1, size)
+        rand_index_2 = random.randint(1, size)
         temp = numbers[rand_index_1]
         numbers[rand_index_1] = numbers[rand_index_2]
         numbers[rand_index_2] = temp
 
-    f = open('B_%d.txt' % size, 'w')
+    f = open('B_%d.txt' % swaps, 'w')
     for number in iter(numbers):
         f.write('%d\n' % number)
     f.close()
 
 if __name__ == "__main__":
-    for i in range(1, 26, 1):
+    for i in range(25, 26, 1):
         print("Position swaps %d" % (2 ** i))
         gen_list(i)
